ba5041f0374f7ba739ba6261d951c417aef2bea3
Ricardo Teixeira <ricardodeveloper@outlook.com>
2019-10-02 11:31:38 -0300

    Back-end Finalizado

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,6 +5,8 @@ const uploadConfig = require('../config/upload')
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 
 const routes = express.Router(); 
@@ -13,10 +15,16 @@ const upload = multer(uploadConfig)
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
+// req.headers = Acessar Header da requisição.
 
 routes.post('/sessions', SessionController.store)
 
+routes.get('/spots', SpotController.index)
 routes.post('/spots', upload.single('thumbnail') , SpotController.store)
+
+routes.get('/dashboard', DashboardController.show)
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes;
 
